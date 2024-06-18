8f7a64a477f30853165b7ea38e050c4639a7c74e
martinlozzano <martinlozzano7@gmail.com>
2024-06-18 19:58:36 -0300

    Se agrego el archivo 'vercel.json'

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,6 @@ function App() {
             <Route path="/checkout_carrito/:checkoutId" element={<CarritoCheckout/>}/> 
             <Route path="/compra_finalizada" element={<CompraFinalizada/>}/> 
             <Route path="/*" element={<NotFound/>}/>
-
           </Routes>
           <Footer />
         </BrowserRouter>
